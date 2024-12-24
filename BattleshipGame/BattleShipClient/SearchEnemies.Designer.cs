b610ee2d244f63bba91710994cda106d4ba0da96
DESKTOP-I1OU07J\Khanh <duykhanh.doc@gmail.com>
2024-12-24 21:48:47 +0700

    Update UI

--- BattleshipGame/BattleShipClient/SearchEnemies.Designer.cs
+++ BattleshipGame/BattleShipClient/SearchEnemies.Designer.cs
@@ -47,8 +47,8 @@
             this.lblSearchEnemy.AutoSize = true;
             this.lblSearchEnemy.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSearchEnemy.ForeColor = System.Drawing.Color.White;
-            this.lblSearchEnemy.Location = new System.Drawing.Point(587, 72);
-            this.lblSearchEnemy.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblSearchEnemy.Location = new System.Drawing.Point(440, 58);
+            this.lblSearchEnemy.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSearchEnemy.Name = "lblSearchEnemy";
             this.lblSearchEnemy.Size = new System.Drawing.Size(311, 55);
             this.lblSearchEnemy.TabIndex = 2;
@@ -59,10 +59,10 @@
             this.btnConnect.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.btnConnect.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btnConnect.ForeColor = System.Drawing.Color.White;
-            this.btnConnect.Location = new System.Drawing.Point(505, 821);
-            this.btnConnect.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnConnect.Location = new System.Drawing.Point(379, 657);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(624, 71);
+            this.btnConnect.Size = new System.Drawing.Size(468, 57);
             this.btnConnect.TabIndex = 6;
             this.btnConnect.Text = "Select this enemy and play";
             this.btnConnect.UseVisualStyleBackColor = false;
@@ -95,14 +95,14 @@
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dgvAvailableEnemies.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dgvAvailableEnemies.Location = new System.Drawing.Point(203, 191);
-            this.dgvAvailableEnemies.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.dgvAvailableEnemies.Location = new System.Drawing.Point(152, 153);
+            this.dgvAvailableEnemies.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvAvailableEnemies.MultiSelect = false;
             this.dgvAvailableEnemies.Name = "dgvAvailableEnemies";
             this.dgvAvailableEnemies.ReadOnly = true;
             this.dgvAvailableEnemies.RowHeadersVisible = false;
             this.dgvAvailableEnemies.RowHeadersWidth = 62;
-            this.dgvAvailableEnemies.Size = new System.Drawing.Size(1263, 582);
+            this.dgvAvailableEnemies.Size = new System.Drawing.Size(947, 466);
             this.dgvAvailableEnemies.TabIndex = 7;
             this.dgvAvailableEnemies.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVAvailableEnemies_CellClick);
             // 
@@ -128,21 +128,21 @@
             // 
             this.txtSearch.BackColor = System.Drawing.Color.White;
             this.txtSearch.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSearch.Location = new System.Drawing.Point(1067, 69);
-            this.txtSearch.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtSearch.Location = new System.Drawing.Point(800, 55);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtSearch.MaxLength = 16;
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(397, 63);
+            this.txtSearch.Size = new System.Drawing.Size(299, 63);
             this.txtSearch.TabIndex = 8;
             this.txtSearch.TextChanged += new System.EventHandler(this.SearchEnemy);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1632, 14);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(1224, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(57, 58);
+            this.pictureBox1.Size = new System.Drawing.Size(43, 46);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
@@ -150,10 +150,10 @@
             // 
             // SearchEnemies
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
-            this.ClientSize = new System.Drawing.Size(1704, 930);
+            this.ClientSize = new System.Drawing.Size(1300, 800);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.dgvAvailableEnemies);
@@ -161,7 +161,7 @@
             this.Controls.Add(this.lblSearchEnemy);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "SearchEnemies";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
